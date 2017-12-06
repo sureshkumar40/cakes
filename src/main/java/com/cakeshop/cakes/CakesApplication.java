672e18d732c0fa84636970a4c49fb424740d53b6
suresh <suresh@suresh-PC>
2017-12-06 18:45:19 +0530

    Commiting

---
+++ src/main/java/com/cakeshop/cakes/CakesApplication.java
@@ -0,0 +1,12 @@
+package com.cakeshop.cakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakesApplication.class, args);
+	}
+}
