016a21a11fdfbe25770daf8f628bf96df1faaa19
suresh <suresh@suresh-PC>
2017-12-06 20:25:03 +0530

    Changes

--- src/main/java/com/cakeshop/cakes/CakesApplication.java
+++ src/main/java/com/cakeshop/cakes/CakesApplication.java
@@ -3,7 +3,7 @@ package com.cakeshop.cakes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.cakeshop.cakes.service"})
 public class CakesApplication {
 
 	public static void main(String[] args) {
